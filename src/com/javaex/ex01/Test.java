a8e8ced8b706cd2f3475ae62fb43566c6a737f11
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 11:27:31 +0900

    test

---
+++ src/com/javaex/ex01/Test.java
@@ -0,0 +1,21 @@
+package com.javaex.ex01;
+
+	public class Test {
+	
+		public static void main(String[] args) {
+			
+			for(int row = 1; row < 7 ; row++) {
+				
+					for(int line = 0 ; line < row; line++) {
+						
+		             System.out.print("*");
+						
+					}
+			    
+			System.out.println();
+				
+		    }
+			
+	}
+	
+}
