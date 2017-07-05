8aa2f2efd869ea1ffbe65d99d1ca5ca8601aa263
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 15:30:24 +0900

    code

--- src/com/javaex/ex01/Prac02.java
+++ src/com/javaex/ex01/Prac02.java
@@ -11,18 +11,20 @@ public class Prac02 {
 			 int num = sc.nextInt();	 
 			 int count = 1;
 				 
-				 for(int row = 1; row <= num; row++) {//열, 입력한 숫자 만큼 높이를 설정
-						 
-								 for(int line = 1; line <= count; line++) {//행, 한줄 입력할 때 마다 증가, count만큼 입력.
-									 
-									 System.out.print(count);
-									 
-								 }
-						 
-					count++;//한 줄 입력될 때마다 1씩 증가시킨다. 
+					 for(int row = 1; row <= num; row++) {//열, 입력한 숫자 만큼 높이를 설정
 							 
-					System.out.println();//입력이 끝나면 엔터 
-				 }
+									 for(int line = 1; line <= count; line++) {//행, 한줄 입력할 때 마다 증가, count만큼 입력.
+										 
+										 System.out.print(count);
+										 
+									 }
+							 
+						count++;//한 줄 입력될 때마다 1씩 증가시킨다. 
+								 
+						System.out.println();//입력이 끝나면 엔터 
+					 }
+					 
+			 sc.close();
 			 
 		 }
 }
