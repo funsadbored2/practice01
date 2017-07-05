6e36e3a179f497492465432c975bc92df813c3b4
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 15:19:12 +0900

    code

--- src/com/javaex/ex01/Prac07.java
+++ src/com/javaex/ex01/Prac07.java
@@ -10,7 +10,7 @@ public class Prac07 {
 		System.out.println("  숫자맞추기 게임  ");
 		System.out.println("==============");
 		
-		int num = (int)(Math.random()*100)+1;
+		int num = (int)(Math.random()*100)+1;//랜덤 값
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -21,25 +21,29 @@ public class Prac07 {
 				int insert = sc.nextInt();
 				
 				
-						if(insert < num) {
+						if(insert < num) {//num보다 낮은 값을 입력한 경우
 							
 							System.out.println("더 높게");
 							
 						}
-						else if(insert > num) {
+						else if(insert > num) {//num보다 높은 값을 입력한 경우
 							
 							System.out.println("더 낮게");
 						
-						} else {
+						} else {//정답을 맞춘 경우
 							
 							System.out.println("정답입니다.");
 							System.out.print("게임을 종료하시겠습니까? (y/n) >> ");
 							String answer = sc.next();
-						    
-								if("y".equals(answer)) {
+							
+						        //질문에 대한 응답 처리를 위해 else 안에 if문 사용
+							
+								if("y".equals(answer)) {//y입력하면 종료
+									
 									System.out.println("종료합니다.");
 									break;
-								} else {
+									
+								} else {//no입력 시 타이틀 다시 불러오고 랜덤값 재입력
 									
 									System.out.println("==============");
 									System.out.println("  숫자맞추기 게임  ");
@@ -50,6 +54,8 @@ public class Prac07 {
 									continue;}
 						  }
 			}
+			
 	   sc.close();	
+	   
 	}
 }
