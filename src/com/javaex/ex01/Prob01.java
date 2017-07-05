013b86c3ce86d0f8026389a097665beed8ed9727
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 11:15:39 +0900

    test

--- src/com/javaex/ex01/Prob01.java
+++ src/com/javaex/ex01/Prob01.java
@@ -1,5 +1,21 @@
 package com.javaex.ex01;
 
-public class Prob01 {
-
+	public class Prob01 {
+	
+		public static void main(String[] args) {
+			
+			for(int row = 1; row < 7 ; row++) {
+				
+					for(int line = 0 ; line < row; line++) {
+						
+		             System.out.print("*");
+						
+					}
+			    
+			System.out.println();
+				
+		    }
+			
+	}
+	
 }
